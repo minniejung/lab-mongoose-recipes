3f04cbe37e35f93183c7e09c17d0fd54e27cc71c
minniejung <94359917+minniejung@users.noreply.github.com>
2022-01-05 20:23:14 +0100

    Solved lab

--- index.js
+++ index.js
@@ -1,23 +1,103 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    createARecipe();
+    insertRecipes();
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+// *************** Create **********************
+
+function createARecipe() {
+  Recipe.create({
+    title: "Tiramisu",
+    level: "Easy Peasy",
+    ingredients: [
+      "4 Eggs",
+      "500g Mascarpone Cheese",
+      "6 cups coffee",
+      "400g Finger cookies",
+      "Cocoa powder",
+    ],
+    cuisine: "Italian",
+    dishType: "dessert",
+    duration: 30,
+    creator: "Someone",
+  })
+    .then((dbResult) => {
+      console.log("** New recipe is created! :", dbResult.title);
+    })
+    .catch((error) => {
+      console.log("Failed! :", error);
+    });
+}
+
+// *************** Insert JSON **********************
+
+const json = require("./data.json");
+// console.log(json);
+
+function insertRecipes() {
+  Recipe.insertMany(json)
+    .then(
+      json.forEach((dbResult) => {
+        console.log("** A New recipe is inserted! :", dbResult.title);
+      })
+    )
+    .then(() => {
+      updateARecipe();
+      deleteARecipe();
+    })
+    .catch((error) => {
+      console.log("Failed! :", error);
+    });
+}
+
+// *************** Update **********************
+
+function updateARecipe() {
+  Recipe.findOneAndUpdate(
+    { title: "Rigatoni alla Genovese" },
+    { duration: 100 },
+    { new: true }
+  )
+    .then((dbResult) => {
+      console.log("** New data is updated !", dbResult);
+    })
+    .catch((error) => {
+      console.log("Failed! :", error);
+    });
+}
+
+// *************** Remove **********************
+
+function deleteARecipe() {
+  Recipe.findOneAndRemove({ title: "Carrot Cake" })
+    .then((dbResult) => {
+      console.log("** Selected data is deleted !", dbResult);
+    })
+    .then(() => {
+      mongoose.disconnect(() => console.log("** DB is closed !"));
+    })
+    .catch((error) => {
+      console.log("Failed! :", error);
+    });
+}
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
-});
+  title:  {type: String, unique: true }, // String is shorthand for {type: String}
+  level: {type: String, enum : ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: [String],
+  cuisine: String,
+  dishType: {type: String, enum : ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]},
+  image: { type: String, default : "https://images.media-allrecipes.com/images/75131.jpg"},
+  duration: { type: Number, default: 0},
+  creator: String,
+  created: {type : Date, default: Date.now()},
+}
+);
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
